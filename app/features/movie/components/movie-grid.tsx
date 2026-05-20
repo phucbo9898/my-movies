7f277e7b45f63c2c9699e986e155fe822f547e99
PhucVN <phucvn@newwave.com.vn>
2026-05-20 16:12:27 +0700

    update UI movie card

--- app/features/movie/components/movie-grid.tsx
+++ app/features/movie/components/movie-grid.tsx
@@ -19,7 +19,7 @@ export function MovieGrid({ movies, className = "" }: MovieGridProps) {
 
   return (
     <div
-      className={`grid grid-cols-2 gap-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 ${className}`}
+      className={`grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 ${className}`}
     >
       {movies.map((movie) => (
         <MovieCard key={movie._id} movie={movie} />
