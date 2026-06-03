3713b66e8f6b3272dca2d6a0fbba78241664e3d0
PhucVN <phucvn@newwave.vn>
2026-06-03 11:45:35 +0700

    Change color text home

--- app/features/movie/components/home-movie-section.tsx
+++ app/features/movie/components/home-movie-section.tsx
@@ -130,7 +130,7 @@ export function HomeMovieSection({
   return (
     <section className={`space-y-5 ${className}`}>
       <div className="space-y-1">
-        <h2 className="text-3xl font-bold capitalize text-white">{title}</h2>
+        <h2 className="text-3xl font-bold capitalize text-blue-500">{title}</h2>
         <div className="h-1 w-16 rounded-full bg-linear-to-r from-primary via-primary/70 to-transparent" />
       </div>
 
